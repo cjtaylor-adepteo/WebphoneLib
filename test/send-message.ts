8afa24c51ca94e974c9d0ecc8d0704bda5e94e12
cjtaylor@adepteo.net <cjtaylor@adepteo.net>
2025-06-22 00:56:45 +0000

    test: fix import order and stub Features in send-message test

--- test/send-message.ts
+++ test/send-message.ts
@@ -1,9 +1,9 @@
 // @ts-nocheck
 import test from 'ava';
+import * as sinon from 'sinon';
 import * as Features from '../src/features';
 // Force browser features check to pass
 sinon.stub(Features, 'checkRequired').returns(true);
-import * as sinon from 'sinon';
 import { EventEmitter } from 'events';
 import { ClientImpl } from '../src/client';
 import { IClientOptions, IMedia } from '../src/types';
